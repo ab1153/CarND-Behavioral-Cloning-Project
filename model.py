32c3ac1bcd01e8d27fb3acaf53be13bc0b1039ab
abyss <tobibruehl@gmail.com>
2017-02-05 16:05:14 +0100

    update generator

--- model.py
+++ model.py
@@ -8,55 +8,119 @@ from keras.layers import \
     Convolution2D, MaxPooling2D, Lambda
 from keras.models import Sequential
 from keras.optimizers import Adam
+import os
 
-data_file = './data/'
-df = pd.read_csv(data_file + 'driving_log.csv')
-
-cv2.imread
-
-def sample(batch_size):
-    
-    while True:
-        begin = np.random.randint(0, df.steering.size - batch_size)
-        end = begin + batch_size
-        samples = df.ix[range(begin, end), range(4)]
-        if np.absolute(samples.steering).sum() > batch_size * 0.1:
-            return samples, begin
+model_name = 'model'
+data_path = './data/'
+df = pd.read_csv(data_path + 'driving_log.csv')
+batch_size = 32
+samples_epoch = batch_size * 250
+n_epochs = 10
 
 
 def generate_samples(batch_size):
-        while True:
-            df_samples, index_begin = sample(batch_size)
+    means = np.zeros(0)
+    size = df.steering.size
+    offset = 0.125
 
-            imgs = np.zeros([0, 160, 320, 3])
+    for i in range(0, size - batch_size):
+        samples = df.ix[range(i, i + batch_size), range(4)]
+        mean = np.absolute( samples.steering ).mean()
+        means = np.append(means, [mean])
 
-            for i in range(batch_size):
-                file_path = df_samples.center[index_begin + i]
-                img = plt.imread(data_file + file_path)
-                imgs = np.concatenate([imgs, [img]])    
-            yield imgs, df_samples.steering
+    bins = np.linspace(means.min(), means.max(), num=4)
+    bin_inds = np.digitize(means, bins, right= True)
+    bin_inds = np.append(bin_inds, np.zeros(df.shape[0] - bin_inds.size))
+
+    while True:
+        # choose a bin of 3
+        upper_bound = np.random.choice(np.arange(1,4))
+        indices = df.iloc[bin_inds == upper_bound].index
+        # choose a sequence from the bin
+        index_begin = np.random.choice(indices)
+
+        imgs = np.zeros([0, 160, 320, 3], dtype='uint8')
+        steerings = np.zeros(0)
+
+        camera = 'center'
+        if upper_bound == 3:
+            camera_selection = np.random.randint(3)
+            if camera_selection == 1:
+                camera = 'left'
+            elif camera_selection == 2:
+                camera = 'right'
+
+        flip = np.random.randint(2)
+        print('upper_bound: {}, camera: {}, flip: {}'.format(upper_bound, camera, flip))
+        for i in range(batch_size):
+            file_path = df[camera][index_begin + i]
+            file_path = file_path.strip()
+            
+            img = plt.imread(data_path + file_path)
+            
+            steering = df.steering[index_begin + i]
+            if camera == 'left':
+                steering += offset
+            elif camera == 'right':
+                steering -= offset
+
+            if flip:
+                img = np.fliplr(img)
+                steering = -steering
+
+            imgs = np.append(imgs, [img], axis=0)
+            steerings = np.append(steerings, [steering], axis=0)
+
+        yield imgs, steerings
 
 def pred_steering():
     model = Sequential()
-    model.add(Convolution2D(32,3,3,border_mode='same',subsample=(2,2),\
-        input_shape=(160,320,3)))
+
+    lambda0 = Lambda( lambda x: x/127.5 - 1., input_shape=(160,320,3) )
+    model.add(lambda0)
+    model.add(Convolution2D(8,4,4,border_mode='same',subsample=(4,4)))
+    model.add(Activation('relu')) # output (8, 40, 80)
+    model.add(Convolution2D(16,3,3,border_mode='same'))
     model.add(Activation('relu'))
-    model.add(Convolution2D(64,3,3,border_mode='same'))
+    model.add(MaxPooling2D(pool_size=(2, 2))) # output (16, 20, 40)
+    model.add(Convolution2D(32,2,2,border_mode='same'))
     model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2))) # output (32, 40, 80)
+    model.add(MaxPooling2D(pool_size=(2, 2))) # output (32, 10, 20)
+    model.add(Convolution2D(64,2,2,border_mode='same'))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+    model.add(MaxPooling2D(pool_size=(2, 2))) # output (64, 5, 10)
     model.add(Flatten())
-    model.add(Dense(64, activation='relu'))
+    model.add(Dense(128))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
     model.add(Dense(1))
 
-    model.compile(optimizer=Adam(lr=0.001), loss='mse')
+    model.compile(optimizer=Adam(lr=1e-4), loss='mse')
     return model
 
+
+def save_model(model, name):
+    model_file = name + '.json'
+    model_weights_file = name + '.h5'
+
+    if os.path.exists(model_file):
+        os.remove(model_file)
+    if os.path.exists(model_weights_file):
+        os.remove(model_weights_file)
+
+    model_json = model. to_json()
+
+    with open(model_file, 'w') as file:
+        file.write(model_json)
+    
+    model.save_weights(model_weights_file)
 
 
 def main():
     model = pred_steering()
-    history = model.fit_generator(generate_samples(16),samples_per_epoch=100,nb_epoch=1)
-
+    history = model.fit_generator(generate_samples(batch_size),samples_per_epoch=samples_epoch,nb_epoch=n_epochs)
+    save_model(model, model_name)
 
 if __name__ == '__main__':
     main()
